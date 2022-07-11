778eec3ffa28cd25a600dcbdf8c44f0619fec743
h0lloway <65232972+h0lloway@users.noreply.github.com>
2022-07-11 12:19:07 +0300

    work updated
    ...

--- js/main.js
+++ js/main.js
@@ -10,35 +10,35 @@ const containHide = () => header.classList.contains('hide');
 window.addEventListener('scroll', () => {
 
 
-  if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-    // scroll down
-    header.classList.add('hide');
-  }
-  else if (scrollPosition() < lastScroll && containHide()) {
-    // scroll up
-    header.classList.remove('hide');
-  }
+	if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+		// scroll down
+		header.classList.add('hide');
+	}
+	else if (scrollPosition() < lastScroll && containHide()) {
+		// scroll up
+		header.classList.remove('hide');
+	}
 
 
-  lastScroll = scrollPosition();
+	lastScroll = scrollPosition();
 });
 
 // ПЛАВНЫЕ ЯКОРЯ
 
 $('a[href*="#"]').on('click', function () {
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 600);
-  return false;
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 600);
+	return false;
 });
 
 // BURGER
 
 $(document).ready(function () {
-  $('.nav__burger').click(function (event) {
-    $('.nav__burger,.nav__list').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
+	$('.nav__burger').click(function (event) {
+		$('.nav__burger,.nav__list').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 });
 
 // SWICH THEME
@@ -65,57 +65,58 @@ const toggleThemeImg = document.getElementById('theme-img');
 
 
 function setDarkTheme() {
-  document.body.classList.add('dark')
-  toggleThemeImg.src = 'img/sun.png'
-  localStorage.theme = 'dark'
+	document.body.classList.add('dark')
+	toggleThemeImg.src = 'img/sun.png'
+	localStorage.theme = 'dark'
 }
 
 function setLightTheme() {
-  document.body.classList.remove('dark')
-  toggleThemeImg.src = 'img/moon.png'
-  localStorage.theme = 'light'
+	document.body.classList.remove('dark')
+	toggleThemeImg.src = 'img/moon.png'
+	localStorage.theme = 'light'
 }
 
 toggleThemeBtn.addEventListener('click', () => {
 
-  if (document.body.classList.contains('dark')) setLightTheme()
-  else setDarkTheme()
+	if (document.body.classList.contains('dark')) setLightTheme()
+	else setDarkTheme()
 })
 
 if (localStorage.theme === 'dark') setDarkTheme()
 
 
-// TOOLTIPS
-
-// tippy('.js-tooltip', {
-//   theme: 'projects-tooltip',
-//   animation: 'fade',
-//   delay: 300,
-//   placement: "top",
-//   allowHTML: !0,
-//   role: "tooltip",
-//   trigger: "mouseenter focus click",
-//   hideOnClick: !0,
-//   maxWidth: 250
-// });
-
-
-
-
 // РАСКРЫТЬ КАРТОЧКИ
 
-const hifgRatingItems = gsap.timeline({ paused: true })
+// const hifgRatingItems = gsap.timeline({ paused: true })
 
-hifgRatingItems
-  .to(".hidden", { duration: .2, opacity: 1, visibility: "visible", display: "inline-flex" })
+// hifgRatingItems
+// 	.to(".hidden", { duration: .2, opacity: 1, visibility: "visible", display: "inline-flex" })
 
-document.querySelector(".more-item-btn").addEventListener("click", function () {
-  document.querySelector(".close-item-btn").classList.add("visible")
-  document.querySelector(".more-item-btn").classList.add("hidden")
-  hifgRatingItems.timeScale(1).play()
-})
-document.querySelector(".close-item-btn").addEventListener("click", function () {
-  document.querySelector(".close-item-btn").classList.remove("visible")
-  document.querySelector(".more-item-btn").classList.remove("hidden")
-  hifgRatingItems.timeScale(2).reverse()
-})
+// document.querySelector(".more-item-btn").addEventListener("click", function () {
+// 	document.querySelector(".close-item-btn").classList.add("visible")
+// 	document.querySelector(".more-item-btn").classList.add("hidden")
+// 	hifgRatingItems.timeScale(1).play()
+// })
+// document.querySelector(".close-item-btn").addEventListener("click", function () {
+// 	document.querySelector(".close-item-btn").classList.remove("visible")
+// 	document.querySelector(".more-item-btn").classList.remove("hidden")
+// 	hifgRatingItems.timeScale(2).reverse()
+// })
+
+
+const showMore = document.querySelector('.more-item-btn');
+const productsLength = document.querySelectorAll('.item-portf').length;
+let items = 6;
+
+showMore.addEventListener('click', () => {
+	items += 3
+	const array = Array.from(document.querySelector('.portfolio__list').children);
+	const visItems = array.slice(0, items);
+
+	visItems.forEach(el => el.classList.add('is-visible'));
+
+
+	if (visItems.length === productsLength) {
+		showMore.style.display = 'none';
+	}
+});
