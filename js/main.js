f1816029a268b3de241eb96231dba2dac452fddd
Dubrovin Maksim <gvozdb27@gmail.com>
2024-02-15 23:34:15 +0300

    work updated

--- js/main.js
+++ js/main.js
@@ -25,11 +25,45 @@ window.addEventListener('scroll', () => {
 
 // ПЛАВНЫЕ ЯКОРЯ
 
-$('a[href*="#"]').on('click', function () {
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 600);
-  return false;
+// $('a[href*="#"]').on('click', function () {
+//   $('html, body').animate({
+//     scrollTop: $($.attr(this, 'href')).offset().top
+//   }, 600);
+//   return false;
+// });
+
+// собираем все якоря; устанавливаем время анимации и количество кадров
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+  animationTime = 600,
+  framesCount = 80;
+
+anchors.forEach(function (item) {
+  // каждому якорю присваиваем обработчик события
+  item.addEventListener('click', function (e) {
+    // убираем стандартное поведение
+    e.preventDefault();
+
+    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    // запускаем интервал, в котором
+    let scroller = setInterval(function () {
+      // считаем на сколько скроллить за 1 такт
+      let scrollBy = coordY / framesCount;
+
+      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+      // и дно страницы не достигнуто
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+      // время интервала равняется частному от времени анимации и к-ва кадров
+    }, animationTime / framesCount);
+  });
 });
 
 // BURGER
@@ -137,3 +171,17 @@ jQuery(document).ready(function () {
     $('html, body').animate({ scrollTop: 0 }, '300');
   });
 });
+
+
+
+// Приветствие по времени
+
+const hours = new Date().getHours();
+let message;
+
+if (hours < 12) message = 'Доброе утро!'
+else message = 'Добрый день!'
+
+if (hours > 16) message = 'Добрый вечер!'
+
+hi.innerText = message;
